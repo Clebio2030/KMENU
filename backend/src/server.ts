8b47511a608ec6fccac54f105da8a3498c7dd1ba
Clebio <134241152+Clebio2030@users.noreply.github.com>
2025-07-30 22:49:51 -0300

    Primeiro commit

---
+++ backend/src/server.ts
@@ -0,0 +1,71 @@
+import 'dotenv/config';
+import gracefulShutdown from "http-graceful-shutdown";
+import https from 'https';
+import fs from 'fs';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cron from "node-cron";
+import { initIO } from "./libs/socket";
+import logger from "./utils/logger";
+import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
+import Company from "./models/Company";
+import BullQueue from './libs/queue';
+import { startQueueProcess } from "./queues";
+import flowBuilderRoutes from './routes/flowBuilderRoutes';
+import app from "./app"; 
+
+const serverApp = express(); 
+
+serverApp.use(bodyParser.json());
+serverApp.use('/flow-builder', flowBuilderRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async (server) => {
+  const companies = await Company.findAll({
+    where: { status: true },
+    attributes: ["id"]
+  });
+
+  const allPromises = companies.map(c => StartAllWhatsAppsSessions(c.id));
+
+  Promise.all(allPromises).then(async () => {
+    await startQueueProcess();
+  });
+
+  if (process.env.REDIS_URI_ACK && process.env.REDIS_URI_ACK !== '') {
+    BullQueue.process();
+  }
+
+  logger.info(`Server started on port: ${PORT}`);
+  initIO(server);
+  gracefulShutdown(server);
+};
+
+if (process.env.CERTIFICADOS === "true") {
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.SSL_KEY_FILE),
+    cert: fs.readFileSync(process.env.SSL_CRT_FILE)
+  };
+
+  const server = https.createServer(httpsOptions, serverApp).listen(PORT, () => {
+    startServer(server);
+    logger.info(`Server started on port: ${PORT} with HTTPS`);
+  });
+} else {
+  const server = serverApp.listen(PORT, () => {
+    startServer(server);
+    logger.info(`Server started on port: ${PORT}`);
+  });
+}
+
+process.on("uncaughtException", err => {
+  console.error(`${new Date().toUTCString()} uncaughtException:`, err.message);
+  console.error(err.stack);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason, p) => {
+  console.error(`${new Date().toUTCString()} unhandledRejection:`, reason, p);
+  process.exit(1);
+});
